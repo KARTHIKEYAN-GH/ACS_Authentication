47626149da3415365ceed6d40bb3b7b6f215053f
karthikeyan <karthikeyan190100@gmail.com>
2025-04-29 10:46:05 +0530

    conflict resolved

--- src/main/java/com/acs/authentication/controller/ACSController.java
+++ src/main/java/com/acs/authentication/controller/ACSController.java
@@ -110,7 +110,18 @@ public class ACSController {
 					return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null));
 				});
 	}
+	
+	@GetMapping("get/{command}")
+	public Mono<ResponseEntity<JsonNode>> listNetworks(@PathVariable String command,
+			@RequestParam Map<String, String> queryParams) {
+		return acsService.callAcsApi(HttpMethod.GET, command, queryParams, null)
 
+				.map(ResponseEntity::ok).onErrorResume(e -> {
+					e.printStackTrace();
+					return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null));
+				});
+	}
+	
 	private ResponseEntity<JsonNode> error(String message) {
 		ObjectNode errorJson = objectMapper.createObjectNode();
 		errorJson.put("error", message);
